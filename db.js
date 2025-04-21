3c3dbe1e23f1257c63c463c51b5119410004e2ec
Tanishk khare <kharetanishk18@gmail.com>
2025-04-21 11:06:18 +0530

    added the database schema

---
+++ db.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+
+//user schema
+const User = new Schema({
+  email: { type: String, unique: true },
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+//course schema
+const Course = new Schema({
+  title: String,
+  decription: String,
+  price: Number,
+  imageUrl: String,
+  creatorId: ObjectId,
+});
+
+//Coursecreator Schema a.k.a Admin
+const Admin = new Schema({
+  email: String,
+  password: String,
+  firstName: String,
+  lastName: String,
+});
+
+//purchases
+const Purchase = new Schema({
+  courseId: ObjectId,
+  userId: ObjectId,
+});
+
+//course Content
+const ContentOfCourse = new Schema({
+  content: String,
+  courseId: ObjectId,
+});
+
+//CREATING MODEL
+const UserModel = mongoose.model("users", User);
+const AdminModel = mongoose.model("admin", Admin);
+const CourseModel = mongoose.model("course", Course);
+const PurchaseModel = mongoose.model("purchasedCourses", Purchase);
+const ContentModel = mongoose.model("courseContent", ContentOfCourse);
+
+//EXPORTING THE MODELS
+module.exports = {
+  UserModel: UserModel,
+  AdminModel: AdminModel,
+  CourseModel: CourseModel,
+  PurchaseModel: PurchaseModel,
+  ContentModel: ContentModel,
+};

109e49adf8eb8eb758646076c5b663301896287b
Tanishk khare <kharetanishk18@gmail.com>
2025-04-28 22:51:57 +0530

    added free content preview route

--- router/course.js
+++ router/course.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
-const { CourseModel } = require("../db");
+const { CourseModel, FreeCourseModel } = require("../db");
 const courseRouter = Router();
 
+//this course preview will be the first page of our application
 courseRouter.get("/preview", async function (req, res) {
   const courses = await CourseModel.find({});
   res.status(200).json({
@@ -11,3 +12,25 @@ courseRouter.get("/preview", async function (req, res) {
 module.exports = {
   courseRouter: courseRouter,
 };
+
+//let the user also know the free courses
+courseRouter.get("/previewFreeContent", async function (req, res) {
+  try {
+    const freeCourses = await FreeCourseModel.find({});
+    console.log(freeCourses);
+    if (!freeCourses) {
+      return res.status(400).json({
+        error: "THERE IS NO FREE COURSES AVAILABLE ",
+      });
+    } else {
+      res.status(200).json({
+        message: "HERE ARE THE FREE COURSES ",
+        freeCourses: freeCourses.map((courses) => courses.courseName),
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      error: "Some error in accessing free courses",
+    });
+  }
+});
